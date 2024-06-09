d2cba571f6b02409b97cdb1ccb8be649536177f0
vinycxuz <vinyby@gmail.com>
2024-06-09 20:36:10 -0300

    feat(authcontroller): create a module signup that create a new user and jwt using bycrypt

---
+++ controller/AuthController.js
@@ -0,0 +1,28 @@
+const User = require('../models/signup.model');
+const { createSecretToken } = require('../utils/secretToken');
+const bycrypt = require('bcrypt');
+
+module.exports.signup = async (req, res) => {
+  try {
+    const { email, username, password, date } = req.body;
+    const existingUser =  await User.findOne({ email });
+
+    if(existingUser){
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = await User.create({
+      email,
+      username,
+      password,
+      date
+    });
+
+    const secretToken = createSecretToken(user._id);
+    res.cookie('secretToken', secretToken, { httpOnly: false, withCredentials: true });
+    res.status(201).json({ message: 'User created successfully', user });
+    next();
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error', error });
+    } 
+  }
