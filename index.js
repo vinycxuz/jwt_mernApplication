c951ac3ab4846b5a846f91e7c2ee15f23028b23f
vinycxuz <vinyby@gmail.com>
2024-06-09 14:28:29 -0300

    feat(index): require all modules we will use, moongose, cors, dotenv

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
-
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const {STRING_CONNECTION} = process.env;
 
 const app = express();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
