51e99322229de4f0491795abe31de4708e58fb2a
vinycxuz <vinyby@gmail.com>
2024-06-09 22:31:47 -0300

    feat(authController): create a module login using comparison in email and see the email for check previous and login after

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -26,3 +26,29 @@ module.exports.signup = async (req, res, next) => {
       res.status(500).json({ message: 'Internal server error', error });
     } 
   }
+
+  module.exports.login = async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+      if(!email || !password){
+        return res.status(400).json({ message: 'Email and password are required' });
+      }
+      const user = await User.findOne({ email });
+      if(!user){
+        return res.status(400).json({ message: 'Invalid email or password' });
+      }
+      const auth = await bycrypt.compare(password, user.password);
+      if(!auth){
+        return res.status(400).json({ message: 'Invalid email or password' });
+      }
+      const secretToken = createSecretToken(user._id);
+      res.cookie('secretToken', secretToken, { 
+        httpOnly: false, 
+        withCredentials: true 
+      });
+      res.status(200).json({ message: 'Login successful', user });
+      next();
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error', error });
+    }
+  }
